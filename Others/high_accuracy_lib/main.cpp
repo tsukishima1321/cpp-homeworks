bc6f5aeb49213f4a14c7b645898322b2bab81878
tsukishima1321 <18001751830@163.com>
2024-04-04 21:54:49 +0800

    update

--- Others/high_accuracy_lib/main.cpp
+++ Others/high_accuracy_lib/main.cpp
@@ -1,28 +1,33 @@
 //
 //
-#include <iostream>
 #include "LargeNum.h"
+#include <iostream>
 
 using namespace std;
 
-int main()
-{
-    #ifdef _WIN32
+int main() {
+#ifdef _WIN32
     system("chcp 65001");
-    #endif
+#endif
     LargeNum a = LargeNum::fromString("1145141919810666");
-    LargeNum b = LargeNum::fromString("1919810114514");;
-    int c;
+    LargeNum b = LargeNum::fromString("1919810114514");
+    LargeNum c;
+    int d = 114514;
+    cin >> c;
+    cout << c * 10 << endl;
     cout << "a+b:" << a + b << endl;
     cout << "a-b:" << a - b << endl;
     cout << "a*b:" << a * b << endl;
     cout << "a/b:" << a / b << endl;
     cout << "a%b:" << a % b << endl;
-    cout << (a > b) << (a < b) << (a >= b) << (a <= b) << (a == b);
+    c = a + 2 * b + d;
+    cout << c << endl;
+    cout << (a > b) << (a < b) << (a >= b) << (a <= b) << (a == b) << endl;
     a = 0;
     a = -a;
     b = 0;
-    cout << (a > b) << (a < b) << (a >= b) << (a <= b) << (a == b);
+    cout << (a > b) << (a < b) << (a >= b) << (a <= b) << (a == b) << endl;
+
     system("pause");
     return 0;
 }
