0446cb8a6b58c52fe06c4bdd2ec58ca133aabc0c
tsukishima1321 <18001751830@163.com>
2024-04-09 14:34:28 +0800

    update

--- Chapter_6/6_4.cpp
+++ Chapter_6/6_4.cpp
@@ -1,12 +1,24 @@
-//实验六：二维数组、字符数组
-//输入一字符串，将该字符串中的大小写字母相互转换。如，假设原字符串的内容为“How are you”，则转换后的内容为“hOW ARE YOU”
+// 实验六：二维数组、字符数组
+// 输入一字符串，将该字符串中的大小写字母相互转换。如，假设原字符串的内容为“How are you”，则转换后的内容为“hOW ARE YOU”
 #include <iostream>
 
-int main()
-{
-    #ifdef _WIN32
+using std::cin, std::cout, std::endl;
+
+int main() {
+#ifdef _WIN32
     system("chcp 65001");
-    #endif
+#endif
+
+    char s[100];
+    cin.getline(s, 100);
+    for (int i = 0; s[i] != '\0'; i++) {
+        if (s[i] >= 'a' && s[i] <= 'z') {
+            s[i] += 'A' - 'a';
+        } else if (s[i] >= 'A' && s[i] <= 'Z') {
+            s[i] += 'a' - 'A';
+        }
+    }
+    cout << s << endl;
 
     system("pause");
     return 0;
--- Chapter_6/6_5.cpp
+++ Chapter_6/6_5.cpp
@@ -1,13 +1,27 @@
-//实验六：二维数组、字符数组
-//编写程序，将两个已知字符串中第二个字符串连接到第一个字符串的后面，并将连接后的第一个字符串的内容输出。
+// 实验六：二维数组、字符数组
+// 编写程序，将两个已知字符串中第二个字符串连接到第一个字符串的后面，并将连接后的第一个字符串的内容输出。
 #include <iostream>
 
-int main()
-{
-    #ifdef _WIN32
-    system("chcp 65001");
-    #endif
+using std::cin, std::cout, std::endl;
 
+int main() {
+#ifdef _WIN32
+    system("chcp 65001");
+#endif
+
+    char a[100];
+    char b[100];
+
+    cin >> a;
+    cin >> b;
+    int i;
+    for (i = 0; a[i] != '\0'; i++);
+    int j;
+    for (j = 0; b[j] != '\0'; j++) {
+        a[i + j] = b[j];
+    }
+    a[i + j] = '\0';
+    cout << a << endl;
     system("pause");
     return 0;
 }
