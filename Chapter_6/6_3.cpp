d4f7eeee1adcd4a60fabd0d6bff51dce55d03ffb
tsukishima1321 <18001751830@163.com>
2024-04-09 14:26:21 +0800

    update

--- Chapter_6/6_3.cpp
+++ Chapter_6/6_3.cpp
@@ -1,15 +1,45 @@
-//实验六：二维数组、字符数组
+// 实验六：二维数组、字符数组
 /*编一个程序，输入一个字符串，将其倒序存放后输出。例如，假设原数组a的内容为“VISUAL C++PROGRAM”，倒序后数组a中的内容为“MAGORP++C LASUIV”。
 要求：不能借助另外一个数组实现倒序存放。
 */
 #include <iostream>
 
-int main()
-{
-    #ifdef _WIN32
-    system("chcp 65001");
-    #endif
+using std::cin,std::cout,std::endl;
 
+int main() {
+#ifdef _WIN32
+    system("chcp 65001");
+#endif
+
+    char s[100];
+    cin >> s;
+    int length;
+    for (length = 0; s[length] != '\0'; length++);
+    for (int i = 0; i < length / 2; i++) {
+        char temp = s[i];
+        s[i] = s[length - i - 1];
+        s[length - i - 1] = temp;
+    }
+    cout << s;
+
+    system("pause");
+    return 0;
+}
+
+//algorithm & string
+#include <algorithm>
+#include <string>
+
+int main_() {
+#ifdef _WIN32
+    system("chcp 65001");
+#endif
+
+    std::string s;
+    cin >> s;
+    int length;
+    std::reverse(s.begin(),s.end());
+    cout << s;
     system("pause");
     return 0;
 }
