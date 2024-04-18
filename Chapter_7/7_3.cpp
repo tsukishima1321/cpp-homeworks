b54eff64eae9b1fba5728ab90e43a3a7bbe58645
tsukishima1321 <18001751830@163.com>
2024-04-18 18:01:06 +0800

    update

--- Chapter_7/7_3.cpp
+++ Chapter_7/7_3.cpp
@@ -2,12 +2,24 @@
 // 利用指针将一个包含10个整数的数组中的最大最小元素进行交换，并输出交换后的内容。10个整数为随机产生的0~100之间的数。
 // （若最大最小元素不唯一，则交换任意两个都算正确，即可以不用特殊考虑不唯一的情况）
 
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
 
 using namespace std;
 
+int getRand(int min, int max) {
+    return (rand() % (max - min + 1)) + min;
+}
+
 int main() {
-    int a[10] = {9, 3, 6, 7, 1, 2, 5, 0, 4, 8};
+    srand((unsigned)time(0));
+    int a[10];
+    for (int i = 0; i < 10; i++) {
+        a[i] = getRand(0, 100);
+        cout << a[i] << " ";
+    }
+    cout << endl;
     int *max = a, *min = a;
     for (int *i = a; i < a + 10; i++) {
         if (*max < *i) {
@@ -17,7 +29,7 @@ int main() {
             min = i;
         }
     }
-    
+
     // 或者：
     /*
     for (int i = 0; i < 10; i++) {
