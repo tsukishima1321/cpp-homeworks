b9c31b0c8cf79ea0485bbcfe9bc947338af1129d
unknown <PC@jd124.www.tongji.edu.cn>
2024-05-06 15:58:52 +0800

    chapter 9

---
+++ Chapter_9/9_1.cpp
@@ -0,0 +1,26 @@
+﻿// 实验九：函数（续）
+// 编写递归函数int  sum(int a[],int n)，其功能是求长度为n的数组的累加和，在主函数中随机产生10个两位数，调用sum函数，求这10个数的和。
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int sum(int a[],int n){
+	if(n == 1){
+		return a[0];
+	}
+	return a[0] + sum(a + 1,n - 1);
+}
+
+int main(){
+	srand((unsigned)time(0));
+	int a[10];
+	for(int i = 0;i < 10;i++){
+		a[i] = 10 + rand() % 90;
+		cout << a[i] << endl;
+	}
+	cout<< sum(a,10) << endl;
+	system("pause");
+	return 0;
+}
---
+++ Chapter_9/9_2.cpp
@@ -0,0 +1,31 @@
+﻿// 实验九：函数（续）
+// 编写函数get_max，其功能是将字符串s中最大字符的地址返回，再编写一个主函数，调用该函数，
+// 将字符串s中从最大字符开始的子串中小写字母转换成大写字母，然后输出新字符串s。
+// 例如，假设s的内容为“qwertyou”，则从最大字符’y’开始的子串为“you”，处理后的s为“qwertYOU”。
+
+#include <iostream>
+
+using namespace std;
+
+char  *get_max(char s[]){
+	char *imax = s;
+	for(char *i = s;*i != '\0';i++){
+		if(*i > *imax){
+			imax = i;
+		}
+	}
+	return imax;
+}
+
+int main(){
+	char s[100];
+	cin.getline(s,100);
+	for(char *i = get_max(s);*i != '\0';i++){
+		if('a' <= *i && *i <= 'z'){
+			*i += 'A' - 'a';
+		}
+	}
+	cout<< s << endl;
+	system("pause");
+	return 0;
+}
