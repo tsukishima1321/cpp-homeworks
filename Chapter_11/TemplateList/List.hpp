7c413c1160a4c8c6cc39cfe09aed0385172a42d8
tsukishima1321 <18001751830@163.com>
2024-05-24 14:49:35 +0800

    update

--- Chapter_11/TemplateList/List.hpp
+++ Chapter_11/TemplateList/List.hpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <utility>
 
 namespace MyList {
     template <typename T>
@@ -12,18 +12,18 @@ namespace MyList {
                 delete p;
                 p = q;
             }
-        };
+        }
         List(const List &list) : head(nullptr) {
             Node *p = list.head;
             while (p != nullptr) {
                 push_back(p->data);
                 p = p->next;
             }
-        };
+        }
         List(List &&list) {
             head = list.head;
             list.head = nullptr;
-        };
+        }
         List &operator=(const List &list) {
             if (this == &list) {
                 return *this;
@@ -41,7 +41,7 @@ namespace MyList {
                 p = p->next;
             }
             return *this;
-        };
+        }
         List &operator=(List &&list) {
             if (this == &list) {
                 return *this;
@@ -55,50 +55,38 @@ namespace MyList {
             head = list.head;
             list.head = nullptr;
             return *this;
-        };
+        }
         List(std::initializer_list<T> list) : head(nullptr) {
             for (auto &i : list) {
                 push_back(i);
             }
-        };
+        }
         List(T *start, T *end) : head(nullptr) {
             for (T *i = start; i != end; i++) {
                 push_back(*i);
             }
-        };
-        void push_back(const T &data) {
+        }
+        void push_back(auto &&data) {
             if (head == nullptr) {
-                head = new Node;
-                head->data = data;
-                head->next = nullptr;
-                head->last = nullptr;
+                head = new Node(std::forward<decltype(data)>(data));
                 return;
             }
             Node *p = head;
             while (p->next != nullptr) {
                 p = p->next;
             }
-            Node *q = new Node;
-            q->data = data;
-            q->next = nullptr;
-            q->last = p;
+            Node *q = new Node(std::forward<decltype(data)>(data), nullptr, p);
             p->next = q;
-        };
-        void push_front(const T &data) {
+        }
+        void push_front(auto &&data) {
             if (head == nullptr) {
-                head = new Node;
-                head->data = data;
-                head->next = nullptr;
-                head->last = nullptr;
+                head = new Node(std::forward<decltype(data)>(data));
                 return;
             }
-            Node *q = new Node;
-            q->data = data;
-            q->next = head;
+            Node *q = new Node(std::forward<decltype(data)>(data), head, nullptr);
             head->last = q;
-            q->last = nullptr;
             head = q;
-        };
+        }
         void pop_back() {
             Node *p = head;
             while (p->next->next != nullptr) {
@@ -106,13 +94,13 @@ namespace MyList {
             }
             delete p->next;
             p->next = nullptr;
-        };
+        }
         void pop_front() {
             Node *p = head;
             head = head->next;
             head->last = nullptr;
             delete p;
-        };
+        }
         std::size_t size() const {
             std::size_t count = 0;
             Node *p = head;
@@ -121,20 +109,24 @@ namespace MyList {
                 p = p->next;
             }
             return count;
-        };
+        }
+
+    protected:
         template <typename _Val>
         class _Iterator;
         friend class _Iterator<T>;
 
     protected:
-        struct Node {
+        class Node {
+        public:
             T data;
             Node *next;
             Node *last;
+            Node() = delete;
+            Node(auto &&data, Node *next = nullptr, Node *last = nullptr) : data(std::forward<decltype(data)>(data)), next(next), last(last){};
         };
         Node *head;
 
-    public:
         template <typename _Val>
         class _Iterator {
         public:
@@ -142,33 +134,33 @@ namespace MyList {
             _Iterator &operator++() {
                 now = now->next;
                 return *this;
-            };
+            }
             _Iterator operator++(int) {
                 _Iterator temp = *this;
                 now = now->next;
                 return temp;
-            };
+            }
             _Iterator &operator--() {
                 now = now->last;
                 return *this;
-            };
+            }
             _Iterator operator--(int) {
                 _Iterator temp = *this;
                 now = now->last;
                 return temp;
-            };
+            }
             _Val &operator*() {
                 return now->data;
-            };
+            }
             _Val *operator->() {
                 return &now->data;
-            };
+            }
             bool operator==(const _Iterator &rhs) const {
                 return now == rhs.now;
-            };
+            }
             bool operator!=(const _Iterator &rhs) const {
                 return now != rhs.now;
-            };
+            }
 
         private:
             friend class List;
@@ -178,30 +170,55 @@ namespace MyList {
                 for (std::size_t i = 0; i < index; i++) {
                     now = now->next;
                 }
-            };
+            }
             _Iterator(const _Iterator &it) {
                 list = it.list;
                 now = it.now;
-            };
+            }
             _Iterator(_Iterator &&it) {
                 list = it.list;
                 now = it.now;
                 it.list = nullptr;
                 it.now = nullptr;
-            };
+            }
             _Iterator(List<_Val> *list, Node *now) {
                 this->list = list;
                 this->now = now;
-            };
+            }
             List<_Val> *list;
             Node *now;
         };
+
+    public:
         using Iterator = _Iterator<T>;
         Iterator begin() {
             return Iterator(this, std::size_t(0));
-        };
+        }
         Iterator end() {
             return Iterator(this, nullptr);
-        };
+        }
+        Iterator erase(Iterator it) {
+            if (it.now == head) {
+                head = head->next;
+                head->last = nullptr;
+                delete it.now;
+                return Iterator(this, head);
+            }
+            Node *p = it.now->last;
+            p->next = it.now->next;
+            if (it.now->next != nullptr) {
+                it.now->next->last = p;
+            }
+            delete it.now;
+            return Iterator(this, p->next);
+        }
+        Iterator insert(Iterator it, auto &&data) { // insert after it
+            Node *p = new Node(std::forward<decltype(data)>(data), it.now->next, it.now);
+            it.now->next = p;
+            if (p->next != nullptr) {
+                p->next->last = p;
+            }
+            return Iterator(this, p);
+        }
     };
 }
--- Chapter_11/TemplateList/main.cpp
+++ Chapter_11/TemplateList/main.cpp
@@ -1,11 +1,11 @@
 //
 //
+#include "LargeNum.h"
 #include "List.hpp"
 #include <iostream>
 
 int main() {
-    MyList::List<int> list{1,2,3,4,5};
-    /*
+    MyList::List<int> list{1, 2, 3, 4, 5};
     list.push_back(1);
     list.push_back(2);
     list.push_back(3);
@@ -18,13 +18,13 @@ int main() {
     for (auto &v : list) {
         v += 1;
         std::cout << v << " ";
-    }*/
-    for (auto &v : list) {
-        v += 1;
-        std::cout << v << " ";
     }
-    int a[10] = {1,2,3,4,5,6,7,8,9,10};
-    MyList::List<int> list2(a, a+10);
+    getchar();
+    MyList::List<LargeNum> list2;
+    LargeNum a(1);
+    list2.push_back(a);
+    list2.push_back(LargeNum(2));
+    list2.push_back(LargeNum(3) + 5);
     for (auto &v : list2) {
         v += 1;
         std::cout << v << " ";
